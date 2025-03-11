74dd2939707b3f96cb9b8fb10d7f9298f575a914
agocan <578843228@qq.com>
2025-03-10 23:47:34 -0400

    继续测试

--- build.sh
+++ build.sh
@@ -6,6 +6,18 @@ git clone --branch v${version} https://github.com/goharbor/harbor.git
 
 cd harbor
 
+
+sed -i "s#VERSIONTAG=dev#VERSIONTAG=${version}#g" ./Makefile 
+sed -i "s#BASEIMAGETAG=dev#BASEIMAGETAG=${version}#g" ./Makefile 
+sed -i "s#PULL_BASE_FROM_DOCKERHUB=true#PULL_BASE_FROM_DOCKERHUB=false#g" ./Makefile 
+sed -i "s#BUILDBIN=false#BUILDBIN=true#g" ./Makefile 
+sed -i 's#--no-cache##g' make/photon/Makefile 
+sed -i 's#GOARCH=amd64#GOARCH=arm64#g' make/photon/exporter/Dockerfile 
+sed -i '9aENV GOPROXY="https://goproxy.io"' make/photon/exporter/Dockerfile 
+sed -i '2aENV GOPROXY="https://goproxy.io"' make/photon/registry/Dockerfile.binary 
+sed -i '2aENV GOPROXY="https://goproxy.io"' tools/swagger/Dockerfile 
+sed -i 's#swagger_linux_amd64#swagger_linux_arm64#g' tools/swagger/Dockerfile
+
 make package_offline
 
-
+ls
